7845806ff4c1962ba85c24cb3afcfb568bc02c1c
Yugant-S <yugantsonwani@gmail.com>
2026-04-20 20:08:47 +0530

    Update PaymentStrategy Interface

--- PaymentStrategy.java
+++ PaymentStrategy.java
@@ -1,17 +1,3 @@
-/**
- * Strategy Interface
- * 
- * This is the core of the Strategy Design Pattern.
- * It declares a common interface for all supported payment algorithms.
- * The PaymentContext uses this interface to call the algorithm defined
- * by a concrete strategy.
- */
 public interface PaymentStrategy {
-
-    /**
-     * Executes the payment using a specific payment method.
-     *
-     * @param amount The amount to be paid (in rupees/currency unit)
-     */
     void pay(int amount);
 }
